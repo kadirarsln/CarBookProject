091d149c6ff7f22a72aea9a8b24317ddc35f2fc6
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-05-03 01:35:20 +0300

    Update RemoveAboutCommandHandler.cs

--- Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
+++ Core/CarBook.Application/Features/CQRS/Handlers/AboutHandlers/RemoveAboutCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CarBook.Application.Features.CQRS.Commands.AboutCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,18 @@ using System.Threading.Tasks;
 
 namespace CarBook.Application.Features.CQRS.Handlers.AboutHandlers
 {
-    internal class RemoveAboutCommandHandler
+    public class RemoveAboutCommandHandler
     {
+        private readonly IRepository<About> _repository;
+
+        public RemoveAboutCommandHandler(IRepository<About> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(RemoveAboutCommand removeAboutCommand)
+        {
+            var value = await _repository.GetByIdAsync(removeAboutCommand.Id);
+            await _repository.RemoveAsync(value);
+        }
     }
 }
