f1b9414983fcffd04aaa54f10435d2de06e3e88f
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-05-05 01:28:28 +0300

    Banner CQRS and Controller added.

--- Presentation/CarBook.WebApi/Program.cs
+++ Presentation/CarBook.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Features.CQRS.Handlers.BannerHandlers;
 using CarBook.Application.Interfaces;
 using CarBook.Persistance.Context;
 using CarBook.Persistance.Repositories;
@@ -14,6 +15,12 @@ builder.Services.AddScoped<GetAboutByIdQueryHandler>();
 builder.Services.AddScoped<CreateAboutCommandHandler>();
 builder.Services.AddScoped<UpdateAboutComandHandler>();
 builder.Services.AddScoped<RemoveAboutCommandHandler>();
+
+builder.Services.AddScoped<GetBannerQueryHandler>();
+builder.Services.AddScoped<GetBannerByIdQueryHandler>();
+builder.Services.AddScoped<CreateBannerCommandHandler>();
+builder.Services.AddScoped<UpdateBannerCommandHandler>();
+builder.Services.AddScoped<RemoveBannerCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
