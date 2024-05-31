cd377662e8ab2de1ae9abd3f99edcb1c10b32137
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-05-31 18:29:38 +0300

    Features CQRS and Controller added.

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/GetFeatureQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using CarBook.Application.Features.Mediator.Queries.FeatureQueries;
+using CarBook.Application.Features.Mediator.Results.FeatureResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+{
+    public class GetFeatureQueryHandler : IRequestHandler<GetFeatureQuery, List<GetFeatureQueryResult>>
+    {
+        private readonly IRepository<Feature> _reposiory;
+
+        public GetFeatureQueryHandler(IRepository<Feature> reposiory)
+        {
+            _reposiory = reposiory;
+        }
+
+        public async Task<List<GetFeatureQueryResult>> Handle(GetFeatureQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _reposiory.GetAllAsync();
+            return values.Select(x => new GetFeatureQueryResult
+            {
+                FeatureID = x.FeatureID,
+                Name = x.Name,
+            }).ToList();
+        }
+    }
+}
