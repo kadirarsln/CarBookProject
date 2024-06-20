7c1b54ab7fd71b5c83b01c61f82a367b2dbfbbd4
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-06-20 18:46:59 +0300

    ContactIndex and ApiConsume

---
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using CarBook.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult ContactIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ContactIndex(CreateContactDto createContactDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDto.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7203/api/Contacts", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ContactIndex", "Default");
+            }
+            return View();
+        }
+    }
+}
