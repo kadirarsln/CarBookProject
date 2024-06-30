487b60c2bac46e188a4c7236496f537aed250556
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-06-30 19:34:12 +0300

    Last3 Blogs Finished

--- Frontends/CarBook.Dto/BlogDtos/ResultLast3BlogsWithAuthors.cs
+++ Frontends/CarBook.Dto/BlogDtos/ResultLast3BlogsWithAuthors.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UdemyCarBook.Dto.BlogDtos
+namespace CarBook.Dto.BlogDtos
 {
     public class ResultLast3BlogsWithAuthors
     {
