fae89ff12499100c5d705d4c88681bda30bd7504
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2024-07-09 17:47:15 +0300

    Resevation Controller, CQRS, CRUD and Index added.

--- Core/CarBook.Application/Features/Mediator/Queries/CarPricingQueries/GetCarPricingWithTimePeriodQuery.cs
+++ Core/CarBook.Application/Features/Mediator/Queries/CarPricingQueries/GetCarPricingWithTimePeriodQuery.cs
@@ -1,14 +1,14 @@
-﻿//using MediatR;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using CarBook.Application.Features.Mediator.Results.CarPricingResults;
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarBook.Application.Features.Mediator.Results.CarPricingResults;
 
-//namespace CarBook.Application.Features.Mediator.Queries.CarPricingQueries
-//{
-//	public class GetCarPricingWithTimePeriodQuery : IRequest<List<GetCarPricingWithTimePeriodQueryResult>>
-//	{
-//	}
-//}
+namespace CarBook.Application.Features.Mediator.Queries.CarPricingQueries
+{
+	public class GetCarPricingWithTimePeriodQuery : IRequest<List<GetCarPricingWithTimePeriodQueryResult>>
+	{
+	}
+}
